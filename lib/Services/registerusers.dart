ce7b264d8de425136c6f4f28676463e8e0ad2cfb
yasminemsalah <101893753+yasminemsalah@users.noreply.github.com>
2023-01-04 19:31:41 +0200

    Update registerusers.dart

--- lib/Services/registerusers.dart
+++ lib/Services/registerusers.dart
@@ -1,1 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:yarab/models/Users/usermodel.dart';
 
+void createUser(
+    {required String name,
+    required String mobile,
+    required String email,
+    required String pass}) async {
+  final docUser = FirebaseFirestore.instance.collection('users').doc();
+  final user = User(
+      id: docUser.id, name: name, mobile: mobile, email: email, pass: pass);
+  final json = user.toJson();
+  await docUser.set(json);
+}
