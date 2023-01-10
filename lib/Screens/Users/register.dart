382ed95f6bef7fd78d3c6c2664988f1bd3bda43e
nesmatamer19 <96889917+nesmatamer19@users.noreply.github.com>
2023-01-10 23:39:21 +0200

    Add files via upload

---
+++ lib/Screens/Users/register.dart
@@ -0,0 +1,210 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:yarab/services/registerusers.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import '../../services/firebaseAuthMethod.dart';
+
+class Register extends StatefulWidget {
+  const Register({Key? key}) : super(key: key);
+
+  @override
+  _MyRegisterState createState() => _MyRegisterState();
+}
+
+class _MyRegisterState extends State<Register> {
+  GlobalKey<FormState> Thekey = GlobalKey<FormState>();
+  final nameController = TextEditingController();
+  final mobileController = TextEditingController();
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
+  void signIn() async {
+    FirebaseAuthMethods(FirebaseAuth.instance).signupwithEmail(
+        email: emailController.text,
+        password: passwordController.text,
+        context: context);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.purple,
+          flexibleSpace: const Image(
+            image: AssetImage('images/login.jpeg'),
+            fit: BoxFit.cover,
+            width: 250.00,
+          ),
+          toolbarHeight: 150,
+        ),
+        body: Stack(
+          children: [
+            Container(
+              padding: EdgeInsets.only(left: 35, top: 30),
+              child: const Text(
+                'Create\nAccount',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 33,
+                    fontWeight: FontWeight.w700),
+              ),
+            ),
+            SingleChildScrollView(
+              child: Container(
+                padding: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.16),
+                child: Form(
+                  key: Thekey,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Container(
+                        margin: const EdgeInsets.only(left: 35, right: 35),
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              controller: nameController,
+                              style: const TextStyle(color: Colors.black),
+                              decoration: InputDecoration(
+                                  fillColor: Colors.grey.shade100,
+                                  filled: true,
+                                  hintText: "Name",
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: const BorderSide(
+                                        color: Colors.purple, width: 3.0),
+                                    borderRadius: BorderRadius.circular(2),
+                                  )),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your Name';
+                                }
+                                return null;
+                              },
+                            ),
+                            const SizedBox(
+                              height: 40,
+                            ),
+                            TextFormField(
+                              controller: mobileController,
+                              style: const TextStyle(color: Colors.black),
+                              decoration: InputDecoration(
+                                  fillColor: Colors.grey.shade100,
+                                  filled: true,
+                                  hintText: "Mobile number",
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: const BorderSide(
+                                        color: Colors.purple, width: 3.0),
+                                    borderRadius: BorderRadius.circular(2),
+                                  )),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your Mobile Number';
+                                }
+                                return null;
+                              },
+                            ),
+                            const SizedBox(
+                              height: 40,
+                            ),
+                            TextFormField(
+                              controller: emailController,
+                              style: const TextStyle(color: Colors.black),
+                              decoration: InputDecoration(
+                                  fillColor: Colors.grey.shade100,
+                                  filled: true,
+                                  hintText: "Email or contact info",
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: const BorderSide(
+                                        color: Colors.purple, width: 3.0),
+                                    borderRadius: BorderRadius.circular(2),
+                                  )),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your Email';
+                                }
+                                return null;
+                              },
+                            ),
+                            const SizedBox(
+                              height: 40,
+                            ),
+                            TextFormField(
+                              controller: passwordController,
+                              style: const TextStyle(color: Colors.black),
+                              obscureText: true,
+                              decoration: InputDecoration(
+                                  fillColor: Colors.grey.shade100,
+                                  filled: true,
+                                  hintText: "Password",
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: const BorderSide(
+                                        color: Colors.purple, width: 3.0),
+                                    borderRadius: BorderRadius.circular(2),
+                                  )),
+                              validator: (value) {
+                                if (value == null || value.isEmpty) {
+                                  return 'Please enter your Password';
+                                }
+                                return null;
+                              },
+                            ),
+                            const SizedBox(
+                              height: 40,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                const Text(
+                                  'Sign Up',
+                                  style: TextStyle(
+                                      color: Colors.black,
+                                      fontSize: 27,
+                                      fontWeight: FontWeight.w700),
+                                ),
+                                CircleAvatar(
+                                  radius: 30,
+                                  backgroundColor: Colors.purple,
+                                  child: IconButton(
+                                      color: Colors.white,
+                                      onPressed: () {
+                                        final String newName =
+                                            nameController.text;
+                                        final String newMobile =
+                                            mobileController.text;
+                                        final String newEmail =
+                                            emailController.text;
+                                        final String newPass =
+                                            passwordController.text;
+                                      
+                                        createUser(
+                                            name: newName,
+                                            mobile: newMobile,
+                                            email: newEmail,
+                                            pass: newPass);
+                                        signIn();
+                                        final form = Thekey.currentState;
+                                        if (form!.validate()) {
+                                          GoRouter.of(context).go('/login');
+                                        }
+                                      },
+                                      icon: const Icon(
+                                        Icons.arrow_forward,
+                                      )),
+                                )
+                              ],
+                            ),
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
