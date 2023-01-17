7363cb29e6f90703b992d86c0bbbfe8b4670c740
yasminemsalah <101893753+yasminemsalah@users.noreply.github.com>
2023-01-17 22:50:50 +0200

    Create profilemodel.dart

---
+++ lib/Models/Users/profilemodel.dart
@@ -0,0 +1,48 @@
+// To parse this JSON data, do
+//
+//     final Users = UsersFromJson(jsonString);
+
+import 'dart:convert';
+
+import 'package:yarab/models/Users/usermodel.dart';
+
+
+
+List<Profile> ProfileFromJson(String str) =>
+    List<Profile>.from(json.decode(str).map((x) => Profile.fromJson(x)));
+
+String ProfileToJson(List<Profile> data) =>
+    json.encode(List<dynamic>.from(data.map((x) => x.toJson())));
+
+class Profile {
+  Profile({
+    this.id,
+    required this.name,
+    required this.mobile,
+    required this.email
+    
+  });
+  String? id;
+  String name;
+  String mobile;
+  String email;
+  
+
+  factory Profile.fromJson(Map<String, dynamic> json) => Profile(
+        id: json["id"],
+        name: json["name"],
+        mobile: json["mobile"],
+        email: json["email"]
+        
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "mobile": mobile,
+        "email": email
+       
+      };
+
+   
+}
