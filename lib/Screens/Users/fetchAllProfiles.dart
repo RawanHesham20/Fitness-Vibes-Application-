f3da598a912f29eb0831d7667e62fa9f00e4f782
yasminemsalah <101893753+yasminemsalah@users.noreply.github.com>
2023-01-19 07:47:40 +0200

    Update fetchAllProfiles.dart

--- lib/Screens/Users/fetchAllProfiles.dart
+++ lib/Screens/Users/fetchAllProfiles.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:yarab/services/profileServices.dart';
-import '../../models/Users/profileModel.dart';
+
 import '../../models/Users/usermodel.dart';
 import '../../services/registerusers.dart';
-import '../welcomepage.dart';
-import 'edit_name.dart';
+import '../models/profilemodel.dart';
+import '../services/profileServices.dart';
+import 'Users/update_profile.dart';
 
 
 class ProfilePage extends StatefulWidget {
@@ -19,19 +19,14 @@ class _ProfilePageState extends State<ProfilePage> {
     return Scaffold(
       backgroundColor: Color(0xFFFFFFFF),
       appBar: PreferredSize(
-      preferredSize: Size.fromHeight(200.0),
+        preferredSize: Size.fromHeight(200.0),
 
-      //preferredSize: Size.fromWidth(200.0),
-    child: AppBar(
-
-        title: Image.asset('images/profileimage.jpg', fit: BoxFit.cover ),
-          toolbarHeight: 200,
-          leadingWidth: 20,
-          
-         
-        backgroundColor: Colors.black
-
-        ),
+        //preferredSize: Size.fromWidth(200.0),
+        child: AppBar(
+            title: Image.asset('images/e926a50f-16f2-45f3-b534-5b3a9fead715.jpg', fit: BoxFit.fill),
+            toolbarHeight: 240,
+            leadingWidth: 20,
+            backgroundColor: Colors.black),
       ),
       body: StreamBuilder<List<Profile>>(
         stream: readProfile(),
@@ -44,119 +39,117 @@ class _ProfilePageState extends State<ProfilePage> {
                 itemCount: 1,
                 itemBuilder: (context, index) {
                   return Card(
-                     child:Column(
-                        children: <Widget>[
-                          CircleAvatar(
-                radius: 50.0,
-                backgroundImage: AssetImage('images/profilee.jpg'),
-                
-              ),
-              Text(
-                (allProfiles[index].name),
-                style: TextStyle(
-                  fontSize: 30.0,
-                  fontFamily: 'Pacifico',
-                  fontWeight: FontWeight.bold,
-                  color: Colors.pink,
-                ),
-              ),
-              Text(
-                'Profile'.toUpperCase(),
-                style: TextStyle(
-                  fontSize: 12.0,
-                  fontFamily: 'SourceSansPro',
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.5,
-                ),
-              ),
-              SizedBox(
-                height: 20.0,
-                width: 150,
-                child: Divider(
-                  color: Colors.black,
-                ),
-              ),
-               InkWell(
-                child: Card(
-                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.sports_gymnastics,
-                      color: Colors.pink,
-                    ),
-                    title:Text(
-                      (allProfiles[index].name),
-                      style: TextStyle(
+                      child: Column(
+                    children: <Widget>[
+                      CircleAvatar(
+                        radius: 50.0,
+                        backgroundImage: AssetImage('images/profilee.jpg'),
+                      ),
+                      Text(
+                        (allProfiles[index].name),
+                        style: TextStyle(
+                          fontSize: 30.0,
+                          fontFamily: 'Pacifico',
+                          fontWeight: FontWeight.bold,
+                          color: Colors.pink,
+                        ),
+                      ),
+                      Text(
+                        'Profile'.toUpperCase(),
+                        style: TextStyle(
+                          fontSize: 12.0,
                           fontFamily: 'SourceSansPro',
-                          fontSize: 20,
-                          color: Colors.black),
-                    ),
-                  ),
-                ),
-                onTap: (){
-                  Navigator.of(context).push(MaterialPageRoute(builder: (context) => EditProfilePage(currentUserId: '',)));
-                  
-                }
-              ),
-              InkWell(
-                child: Card(
-                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.phone,
-                      color: Colors.pink,
-                    ),
-                    title: Text(
-                      (allProfiles[index].mobile),
-                      style: TextStyle(
-                          fontFamily: 'SourceSansPro',
-                          fontSize: 20,
-                          color: Colors.black),
-                    ),
-                  ),
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 2.5,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20.0,
+                        width: 150,
+                        child: Divider(
+                          color: Colors.black,
+                        ),
+                      ),
+                      InkWell(
+                          child: Card(
+                            margin: EdgeInsets.symmetric(
+                                vertical: 10.0, horizontal: 25.0),
+                            child: ListTile(
+                              leading: Icon(
+                                Icons.sports_gymnastics,
+                                color: Colors.pink,
+                              ),
+                              title: Text(
+                                (allProfiles[index].name),
+                                style: TextStyle(
+                                    fontFamily: 'SourceSansPro',
+                                    fontSize: 20,
+                                    color: Colors.black),
+                              ),
+                            ),
+                          ),
+                          onTap: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => UpdateRecord(
+                                      studentKey: "",
+                                    )));
+                          }),
+                      InkWell(
+                        child: Card(
+                          margin: EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 25.0),
+                          child: ListTile(
+                            leading: Icon(
+                              Icons.phone,
+                              color: Colors.pink,
+                            ),
+                            title: Text(
+                              (allProfiles[index].mobile),
+                              style: TextStyle(
+                                  fontFamily: 'SourceSansPro',
+                                  fontSize: 20,
+                                  color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      ),
+                      InkWell(
+                        child: Card(
+                          margin: EdgeInsets.symmetric(
+                              vertical: 10.0, horizontal: 25.0),
+                          child: ListTile(
+                            leading: Icon(
+                              Icons.email,
+                              color: Colors.pink,
+                            ),
+                            title: Text(
+                              (allProfiles[index].email),
+                              style: TextStyle(
+                                  fontFamily: 'SourceSansPro',
+                                  fontSize: 20,
+                                  color: Colors.black),
+                            ),
+                          ),
+                        ),
+                      ),
 
-                ),
-              ),
-              InkWell(
-                child: Card(
-                  margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  child: ListTile(
-                    leading: Icon(
-                      Icons.email,
-                      color: Colors.pink,
-                    ),
-                    title:Text(
-                      (allProfiles[index].email),
-                      style: TextStyle(
-                          fontFamily: 'SourceSansPro',
-                          fontSize: 20,
-                          color: Colors.black),
-                    ),
-                  ),
-                ),
-                
-              ),
-              
-                          // Center(
-                          //   child: Text('Name: '),
-                          //   ),
-                          //     Text(allProfiles[index].name),
+                      // Center(
+                      //   child: Text('Name: '),
+                      //   ),
+                      //     Text(allProfiles[index].name),
 
-                          // Center(
-                          //   child: Text('Email: '),
-                          //   ),
-                          //     Text(allProfiles[index].email),
+                      // Center(
+                      //   child: Text('Email: '),
+                      //   ),
+                      //     Text(allProfiles[index].email),
 
-                          //  Center(
-                          //    child: Text('Mobile: '),
-                          // ),
-                          // Text(allProfiles[index].mobile),
-                        ],
-                      )
-                  
-                  );
- 
+                      //  Center(
+                      //    child: Text('Mobile: '),
+                      // ),
+                      // Text(allProfiles[index].mobile),
+                    ],
+                  ));
                 },
               ),
             );
@@ -166,8 +159,8 @@ class _ProfilePageState extends State<ProfilePage> {
             );
           }
         },
-    ),
-  
+      ),
     );
+   
   }
 }
